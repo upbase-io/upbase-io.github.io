1b91493eaa4e5e26614cea8bca94b3f5f536a8f3
congphuong <congphuong32@gmail.com>
2021-03-28 23:53:01 +0700

    fix path

--- _nuxt/static/1616949334/features/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fupbase-landing-page\u002F_nuxt\u002Fstatic\u002F1616949334",layout:"default",error:a,serverRendered:true,routePath:"\u002Ffeatures",config:{app:{basePath:"\u002Fupbase-landing-page\u002F",assetsPath:"\u002Fupbase-landing-page\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"792233d4"}}}}(null));
